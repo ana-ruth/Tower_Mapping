f6649286455ad41a1f1b345489cea5363d31ec97
ana-ruth <114896716+ana-ruth@users.noreply.github.com>
2025-06-11 18:10:50 -0400

    fix: removed ping tester

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Ping from "components/PingTester.tsx"
 
 
 export default function Home() {
@@ -11,7 +10,6 @@ export default function Home() {
 
         <h1 className = "text-6xl">Automated Tower Mapping</h1>
 
-        <Ping/>
 
 
       </main>
